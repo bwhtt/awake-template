a5cde5039d6301fb0676c65f9f6a6279652fe31b
Brian W <13488912+bwhtt@users.noreply.github.com>
2020-03-21 19:12:23 -0400

    testing edits

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
-    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
+    <site-hero :title="Bake" :subtitle="cake" :image="featureImage">
       <button
         class="button is-primary"
         @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
       >
-        Subscribe To Newsletter
+        Testing
       </button>
     </site-hero>
     <main-section theme="one-column">
