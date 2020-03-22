25fc3940b01501dc38ef21cb54df318c2c51d05d
Brian W <13488912+bwhtt@users.noreply.github.com>
2020-03-21 21:07:06 -0400

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -15,7 +15,7 @@ export default {
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
-    id: process.env.UA-159903554-1
+    id: 'UA-159903554-1'
   },
   mainMenu: [
     {
